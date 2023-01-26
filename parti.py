ed8347ef3b8324e420342a0ab9cc7aebc9cf0e53
juliahanssen <juliahanssen26@gmail.com>
2023-01-26 09:53:40 +0100

    lagde parti klasse

---
+++ parti.py
@@ -0,0 +1,36 @@
+class Parti:
+    def __init__(self, samlet_verdi, samlet_poeng, stemmer):
+        self._parti = []
+        self._samlet_verdi = samlet_verdi
+        self._samlet_poeng = samlet_poeng
+        self._stemmer = stemmer
+    
+    def hent_politiker(self, etternavn):
+        # ta inn etternavn og returnere politkeren
+        for politiker in self._parti:
+            if etternavn == etternavn:
+                pass
+            # return poltiker 
+            
+    def hent_samlet_verdi(self):
+        return self._samlet_verdi
+
+    def hent_samlet_poeng(self):
+        return self._samlet_poeng
+
+    def hent_stemmer(self):
+        return self._stemmer
+
+    def oppdater_stemmer(self):
+        pass
+    # self._stemmer - oppdater_samlet_verdi 
+
+    def oppdatert_samlet_verdi(self):
+        for politker in self._parti:
+            pass
+
+    def oppdater_parti(self):
+        pass
+
+    def fjern_politiker(self):
+        pass
