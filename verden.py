593d03ec4da67f02aa4919d9a7b5f81f0b916dfe
Christens <christens@viken.no>
2023-01-26 12:47:53 +0100

    poeng oppdatering

--- verden.py
+++ verden.py
@@ -15,11 +15,12 @@ fornavn = []
 
 for politiker in storting:
     if politiker["fornavn"] not in fornavn:
-        liste_med_politikere.append(Politiker(politiker["etternavn"],politiker["fornavn"],politiker["verdi"],politiker["parti"]["navn"],0))
+        liste_med_politikere.append(Politiker(politiker["etternavn"],politiker["fornavn"],politiker["verdi"],politiker["parti"]["navn"]))
         fornavn.append(politiker["fornavn"])
 
 for politiker in regjering:
     if politiker["fornavn"] not in fornavn:
-        liste_med_politikere.append(Politiker(politiker["etternavn"],politiker["fornavn"],politiker["verdi"],politiker["parti"]["navn"],0))
+        liste_med_politikere.append(Politiker(politiker["etternavn"],politiker["fornavn"],politiker["verdi"],politiker["parti"]["navn"]))
         fornavn.append(politiker["fornavn"])
+
 
