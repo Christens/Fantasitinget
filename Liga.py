c1ecd035a45de12a204c62bc4d324f49315bd4b0
jhub04 <122024331+jhub04@users.noreply.github.com>
2023-01-26 12:52:20 +0100

    Utvidelse liga.py

--- Liga.py
+++ Liga.py
@@ -4,3 +4,18 @@ class Liga:
 
     def hent_liga(self):
         return self._liga
+
+    def ranger_lag(self):
+        bibliotek = {}
+        for parti in self._liga:
+            bibliotek[parti] = parti.hent_samlet_poeng()
+        
+        sortert_bib = dict(sorted(bibliotek.items(), key=lambda x: x[1], reverse=True))
+
+        for parti, poeng in sortert_bib.items():
+            print(f"{parti}: {poeng}")
+
+    def oppdater_liga(self, liste_med_partier):
+        for parti in liste_med_partier:
+            if parti.hent_status == True:
+                self._liga.append(parti)
