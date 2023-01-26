684ae35469a61f833b4144fd99adf3f4bdc25b5f
Christens <christens@viken.no>
2023-01-26 11:08:41 +0100

    videre jobb med verden

--- verden.py
+++ verden.py
@@ -2,4 +2,14 @@ import json
 from parti import Parti
 from politiker import Politiker
 
-fil_storting = open("")
+fil_storting = open("storting.json")
+storting = json.load(fil_storting)
+politikere_paa_storting = storting["dagensrepresentanter_oversikt"]["dagensrepresentanter_liste"]["dagensrepresentant"]
+
+fil_regjering = open("regjering.json")
+regjering = json.load(fil_regjering)
+
+liste_med_politikere = []
+
+for politiker in politikere_paa_storting:
+    #if politiker["etternavn"]
